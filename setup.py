9cb0d2a538d8f260b4db1019a1e8d78712353272
Rugbeat90999 <111895773+Rugbeat90999@users.noreply.github.com>
2025-04-15 09:25:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='CommonLib',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages("./__init__.py"),
     install_requires=[
         # List your dependencies here
     ],
