3e06b046509ccb2cd67f3a3d4071b8d232bdd9fb
Rugbeat90999 <rugbeat909@gmail.com>
2025-04-15 09:43:50 -0500

    try again at setup

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CommonLib',
-    version='0.1',
-    packages=find_packages(include=("CommonLib")),
-    install_requires=[
-        # List your dependencies here
-    ],
+  name='CommonLib',
+  version='1.0.0',
+  packages=find_packages(include=("CommonLib")),
+  install_requires=[],  # add dependencies like ['requests']
+  author='Rugbeat90999',
+  author_email='rugbeat909@gmail.com',
+  description='A library of different functions and classes for my projects',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/Rugbeat90999/CommonLib',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.12',
 )
